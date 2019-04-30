15c058ca4d91f11a97f61e0aa69079ea51c5bde7
Dustin Haefele <Dustin.haefele@gmail.com>
2019-04-30 15:11:43 -0400

    added turtle Movements

--- HoF.js
+++ HoF.js
@@ -1,28 +1,28 @@
 'use strict';
 function repeat(fn, n){
-for(let i = 0; i <= n; i++){
+  for(let i = 0; i <= n; i++){
     fn();
-}
+  }
 }
 
 function hello(){
-console.log('Hello world');
+  console.log('Hello world');
 }
 
 function goodbye(){
-console.log('Goodbye world');
+  console.log('Goodbye world');
 }
-repeat(hello,5);
-repeat(goodbye,5);
+/*repeat(hello,5);
+repeat(goodbye,5);*/
 
 function filter(arr, fn) {
-    let newArr = [];
-    for(let i = 0; i < arr.length; i++){
-       if(fn(arr[i])=== true){
-           newArr.push(arr[i]);
-       } 
-    }
-    return newArr;
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    if(fn(arr[i])=== true){
+      newArr.push(arr[i]);
+    } 
+  }
+  return newArr;
 }
 
 // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
@@ -32,28 +32,39 @@ const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
 // We use your `filter` function here, capturing a new array into `filteredNames`
 // comprised of names that only begin with 'R'
 const filteredNames = filter(myNames, function(name) {
-    // This is known as a "predicate function" - it's a function that 
-    // only returns a boolean
-    return name[0] === 'R';
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
 });
 
 console.log(filteredNames) // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
 
 function hazardWarningCreator(typeOfWarning){
-    let warningCounter = 0;
-    return function (location){
-        warningCounter++;
-        console.log(`Danger! There is a ${typeOfWarning} hazard at ${location}!`);
-        console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
-    }
+  let warningCounter = 0;
+  return function (location){
+    warningCounter++;
+    console.log(`Danger! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  }
 }
 const rocksWarning = hazardWarningCreator('Rocks on the road!');
 const tornadoWarning = hazardWarningCreator('Tornado approaching your area!');
 const floodWarning = hazardWarningCreator('Flooding in your area!');
 
-rocksWarning('Chicago');
+/*rocksWarning('Chicago');
 tornadoWarning('Mclean County');
 floodWarning('Bloomington-Normal');
 rocksWarning('Athens');
-floodWarning('Atlanta');
+floodWarning('Atlanta');*/
+
+function turtleMovement(arr) {
+   
+  arr
+  //filter looks at each esement and if the function equals true it pushes into a new array
+    .filter(element => element[0] >= 0 && element[1] >= 0)
+    .map(element => element[0] + element[1])
+    .forEach((element, i) => console.log(`Movement ${i+1}: ${element} steps`));
+}
+
+console.log(turtleMovement([[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]));
